90909952fc266864f905a807c63789d38afd78db
TimDunphy <timothy.dunphy@gmail.com>
2020-05-01 13:45:06 +0100

    api route formatting.

--- app/routes/api.js
+++ app/routes/api.js
@@ -10,11 +10,16 @@ fetchAllData().then(val => data = val)
 router.get('/', async (req, res) => {
 
     const options = {
-        lowerYear: (req.query.l !== '' && req.query.l !== undefined) ? parseFloat(req.query.l) : 1860,
-        upperYear: (req.query.u !== '' && req.query.u !== undefined) ? parseFloat(req.query.u) : new Date().getFullYear(),
-        incGenres: (req.query.ig !== 'all' && req.query.ig !== undefined) ? req.query.ig.split(',') : undefined,
-        excGenres: (req.query.eg !== 'none' && req.query.eg !== undefined) ? req.query.eg.split(',') : undefined,
-        excStyles: (req.query.es !== 'none' && req.query.es !== undefined) ? req.query.es.split(',') : undefined,
+        lowerYear: (req.query.l !== '' && req.query.l !== undefined) ?
+            parseFloat(req.query.l) : 1860,
+        upperYear: (req.query.u !== '' && req.query.u !== undefined) ?
+            parseFloat(req.query.u) : new Date().getFullYear(),
+        incGenres: (req.query.ig !== 'all' && req.query.ig !== undefined) ?
+            req.query.ig.split(',') : undefined,
+        excGenres: (req.query.eg !== 'none' && req.query.eg !== undefined) ?
+            req.query.eg.split(',') : undefined,
+        excStyles: (req.query.es !== 'none' && req.query.es !== undefined) ?
+            req.query.es.split(',') : undefined,
         excUndefStyles: req.query.eus,
         mode: req.query.m,
         videos: req.query.v
