ab8702603ae7c108c33021d7dbbbee38f32b9c72
TimDunphy <timothy.dunphy@gmail.com>
2020-05-15 20:07:46 +0100

    Staged deletions from old file structure.

--- app/helpers/currentUser.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-const currentUser = () => {
-    return process.env.DUMMY_USER_ID
-}
-
-module.exports = currentUser()
--- app/helpers/getReleasesFromIDs.js
+++
@@ -1,8 +0,0 @@
-const getReleases = (releases, ids, keys) => {
-    return releases.reduce((acc, curr) => {
-        if (ids.indexOf(curr.id) !== -1) acc.push(curr)
-        return acc
-    }, [])
-}
-
-module.exports = getReleases
